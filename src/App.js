f3121b84fc80ce486d762035aaf1549e2612f129
Shadman Khan <shaad2209@gmail.com>
2023-04-20 10:55:04 +0530

    file upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+        <img src={logo} className="App-logo" alt="logo" />
+
+      <h1 classs="heading">Benefits Of Using React...</h1>
+      <p>1. Components-based-architecture</p>
+      <p>2. Virtual DOM for efficient updates</p>
+      <p>3. Rich ecosystem and community</p>
+      <p>4. Cross-platform development</p>
+      <p>5. Strong community support</p>
+      <button class="btn">Get Started...</button>
+    </div>
+  );
+}
+
+export default App;
